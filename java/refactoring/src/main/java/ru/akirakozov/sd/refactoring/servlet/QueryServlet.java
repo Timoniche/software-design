5b3ea889a37c9b01418f29022ac6c1618e56b5b6
Dmitrii Dulaev <dmitriidulaev@gmail.com>
2020-10-29 02:27:03 +0300

    copy-paste in switch decreased processOp(...) function implemented

--- java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
+++ java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
@@ -14,82 +14,61 @@ import static ru.akirakozov.sd.refactoring.database.ControllerDB.getConnection;
  * @author akirakozov
  */
 public class QueryServlet extends AbstractServlet {
+
+    private void processOp(String header, String sql, HttpServletResponse response, String op) {
+        try {
+            try (Connection c = getConnection()) {
+                Statement stmt = c.createStatement();
+                ResultSet rs = stmt.executeQuery(sql);
+                ArrayList<String> log = new ArrayList<>();
+                log.add(header);
+                switch (op) {
+                    case "max":
+                    case "min":
+                        GetProductsServlet.printResultSet(rs, log);
+                        break;
+                    case "sum":
+                    case "count":
+                        if (rs.next()) {
+                            log.add(rs.getString(1));
+                        }
+                        break;
+                    default: break;
+                }
+                logHttp(log, response);
+                rs.close();
+                stmt.close();
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String command = request.getParameter("command");
 
         switch (command) {
             case "max":
-                try {
-                    try (Connection c = getConnection()) {
-                        Statement stmt = c.createStatement();
-                        ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1");
-                        ArrayList<String> log = new ArrayList<>();
-                        log.add("<h1>Product with max price: </h1>");
-                        GetProductsServlet.printResultSet(rs, log);
-                        logHttp(log, response);
-                        rs.close();
-                        stmt.close();
-                    }
-
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
+                String sqlMax = "SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1";
+                String headerMax = "<h1>Product with max price: </h1>";
+                processOp(headerMax, sqlMax, response, "max");
                 break;
             case "min":
-                try {
-                    try (Connection c = getConnection()) {
-                        Statement stmt = c.createStatement();
-                        ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1");
-                        ArrayList<String> log = new ArrayList<>();
-                        log.add("<h1>Product with min price: </h1>");
-                        GetProductsServlet.printResultSet(rs, log);
-                        logHttp(log, response);
-                        rs.close();
-                        stmt.close();
-                    }
-
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
+                String sqlMin = "SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1";
+                String headerMin = "<h1>Product with min price: </h1>";
+                processOp(headerMin, sqlMin, response, "min");
                 break;
             case "sum":
-                try {
-                    try (Connection c = getConnection()) {
-                        Statement stmt = c.createStatement();
-                        ResultSet rs = stmt.executeQuery("SELECT SUM(price) FROM PRODUCT");
-                        ArrayList<String> log = new ArrayList<>();
-                        log.add("Summary price: ");
-                        if (rs.next()) {
-                            log.add(rs.getString(1));
-                        }
-                        logHttp(log, response);
-                        rs.close();
-                        stmt.close();
-                    }
-
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
+                String sqlSum = "SELECT SUM(price) FROM PRODUCT";
+                String headerSum = "Summary price: ";
+                processOp(headerSum, sqlSum, response, "sum");
                 break;
             case "count":
-                try {
-                    try (Connection c = getConnection()) {
-                        Statement stmt = c.createStatement();
-                        ResultSet rs = stmt.executeQuery("SELECT COUNT(*) FROM PRODUCT");
-                        ArrayList<String> log = new ArrayList<>();
-                        log.add("Number of products: ");
-                        if (rs.next()) {
-                            log.add(rs.getString(1));
-                        }
-                        logHttp(log, response);
-                        rs.close();
-                        stmt.close();
-                    }
-
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
+                String sqlCount = "SELECT COUNT(*) FROM PRODUCT";
+                String headerCount = "Number of products: ";
+                processOp(headerCount, sqlCount, response, "count");
                 break;
             default:
                 response.getWriter().println("Unknown command: " + command);
